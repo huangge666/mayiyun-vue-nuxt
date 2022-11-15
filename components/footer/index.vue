471c261238cd7931703261ffcb6ca2af4210f6b7
Pan.Huang <740890944@qq.com>
2022-11-15 11:31:30 +0800

    初始化文件

---
+++ components/footer/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-center py-2">
+    <a href="https://beian.miit.gov.cn" target="_blank">Copyright© 2020  苏ICP备19010036号-1</a>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: #212529;
+  text-decoration: none;
+}
+</style>
